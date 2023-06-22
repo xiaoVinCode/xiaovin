7941ad9b017ad81c05d9279a58b6e03470898dc8
v_yangjiahao01 <v_yangjiahao01@baidu.com>
2023-06-22 17:00:19 +0800

    GitHub的部署文件

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -18,7 +18,7 @@ export default hopeTheme({
     repo: "https://gitee.com/xiaoVin/xiaovin",
     // repo: "vuepress-theme-hope/vuepress-theme-hope",
     docsDir: "docs",
-    docsBranch: "master",
+    docsBranch: "main",
     // 纯净模式：https://theme-hope.vuejs.press/zh/guide/interface/pure.html
     // pure: true,
 
