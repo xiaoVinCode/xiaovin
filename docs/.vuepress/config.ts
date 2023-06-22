84c39f5a4c67f993998201ee113179bc9fd5a309
v_yangjiahao01 <v_yangjiahao01@baidu.com>
2023-06-22 17:44:42 +0800

    GitHub的部署文件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,7 +4,7 @@ import {searchPlugin} from "@vuepress/plugin-search";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-    base: "/",
+    base: "/xiaovin/",
 
     lang: "zh-CN",
     title: "文档演示",
